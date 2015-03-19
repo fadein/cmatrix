a534806627179766acc11a2f600c709920eafdc3
Erik Falor (fadein) <ewfalor@gmail.com>
2015-03-18 19:15:07 -0600

    cleanup usage text

--- cmatrix.c
+++ cmatrix.c
@@ -124,20 +124,20 @@ RETSIGTYPE c_die(char *msg, ...)
 
 void usage(void)
 {
-    printf(" Usage: cmatrix -[abBfhlsVx] [-u delay] [-C color]\n");
-    printf(" -a: Asynchronous scroll\n");
-    printf(" -b: Bold characters on\n");
-    printf(" -B: All bold characters (overrides -b)\n");
-    printf(" -f: Force the linux $TERM type to be on\n");
-    printf(" -l: Linux mode (uses matrix console font)\n");
-    printf(" -o: Use old-style scrolling\n");
-    printf(" -h: Print usage and exit\n");
-    printf(" -n: No bold characters (overrides -b and -B, default)\n");
-    printf(" -s: \"Screensaver\" mode, exits on first keystroke\n");
-    printf(" -x: X window mode, use if your xterm is using mtx.pcf\n");
-    printf(" -V: Print version information and exit\n");
-    printf(" -u delay (0 - 10, default 4): Screen update delay\n");
-    printf(" -C [color]: Use this color for matrix (default green)\n");
+    printf(" Usage: cmatrix -[abBfhlsVx] [-u delay] [-C color]\n"
+	    " -a: Asynchronous scroll\n"
+	    " -b: Bold characters on\n"
+	    " -B: All bold characters (overrides -b)\n"
+	    " -f: Force the linux $TERM type to be on\n"
+	    " -l: Linux mode (uses matrix console font)\n"
+	    " -o: Use old-style scrolling\n"
+	    " -h: Print usage and exit\n"
+	    " -n: No bold characters (overrides -b and -B, default)\n"
+	    " -s: \"Screensaver\" mode, exits on first keystroke\n"
+	    " -x: X window mode, use if your xterm is using mtx.pcf\n"
+	    " -V: Print version information and exit\n"
+	    " -u delay (0 - 10, default 4): Screen update delay\n"
+	    " -C [color]: Use this color for matrix (default green)\n");
 }
 
 void version(void)
@@ -635,3 +635,4 @@ int main(int argc, char *argv[])
 
 }
 
+/* vim: set tabstop=8: */
